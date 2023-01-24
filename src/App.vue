950198e8ce894c07292c111f7d170fce0b0e0ab8
GiseleCavalli <giselecavalli28@hotmail.com>
2023-01-24 19:18:35 -0300

    Atualizando data

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 
 <template>
-  <ul>
-    <li v-for="feriado in feriados"> {{ format(new Date(2014, 1, 11), 'MM/dd/yyyy') }} 
-    </li>
-  </ul>
+  <div>
+    <div v-for="feriado in feriados"> 
+      {{  feriado.name }} - {{ formatDate({year: feriado.date.datetime.year, month: feriado.date.datetime.month, day: feriado.date.datetime.day}) }}
+    </div>
+  </div>
 </template>
 
 <script>
 import feriados from '../service/feriado';
-import { format, compareAsc } from 'date-fns';
+import { format } from 'date-fns';
 
 export default{
 
@@ -24,26 +25,19 @@ export default{
 
           this.feriados = resposta.data.response.holidays;
 
-
-          // let nomeFeriado = '';
-          // let diaFeriado = '';
-          // let anoFeriado = '';
-          // let mesFeriado = '';
-
-          // resposta.data.response.holidays.forEach(ferias => {
-          //   nomeFeriado += `${ferias.name}` 
-          //   diaFeriado += `${ferias.date.datetime.day}`
-          //   mesFeriado += `${ferias.date.datetime.month}` 
-          //   anoFeriado += `${ferias.date.datetime.year}`
-          // });
-          
-          // this.feriados = nomeFeriado + diaFeriado + anoFeriado + mesFeriado;
-
         }).catch(error => {
           console.log('Vixe')
           console.log(error)
         })
+    },
+
+    methods:{
+      formatDate({year, month, day}){
+        return format(new Date(`${year}-${month}-${day}`), 'dd/MM/yyyy')
+      }
+
     }
+
 }
 </script>
 
