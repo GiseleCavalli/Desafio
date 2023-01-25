8ae0f7ac94dbc498b0702595ddd1ebd4b02bb72c
GiseleCavalli <giselecavalli28@hotmail.com>
2023-01-25 13:03:55 -0300

    Finalizando filtro de mes e dia

--- service/feriado.js
+++ service/feriado.js
@@ -2,6 +2,7 @@ import { http } from "./config";
 
 export default{
     listar:() => {
-        return http.get('holidays?api_key=e7bd8f64e9bf413401a4c1149bc7a73d2344a221&country=br&year=2023&language=pt')
+        // return http.get('holidays?api_key=e7bd8f64e9bf413401a4c1149bc7a73d2344a221&country=br&year=2023&language=pt')
+        return http.get('holidays?api_key=1ba0e18c59501a2e046bae220f78e2ec634c5e6f&country=br&year=2023&language=pt')
     }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 
 <template>
-  <button v-on:click="filtrarMes(6)">Nhoi</button>
   <div>
+    <select v-model="selectedDay" :items="days" @change=filtrarDia(selectedDay)>
+      <option v-for="day, index in days" :key="index" :value="day.id">
+        {{ day.text }}
+      </option>
+    </select>
+
+    <select v-model="selectedMonth" :items="months" @change=filtrarMes(selectedMonth)>
+      <option v-for="month, index in months" :key="index" :value="month.id">
+        {{ month.text }}
+      </option>
+    </select>
+
     <div v-for="feriado in feriados">
-      {{ feriado.name }} - {{
-        formatDate({ year: feriado.date.datetime.year, month: feriado.date.datetime.month, day:
-          feriado.date.datetime.day
-      }) }}
+      {{ feriado.name }} - {{ formatDate({ year: feriado.date.datetime.year, month: feriado.date.datetime.month, day:
+          feriado.date.datetime.day}) }}
     </div>
   </div>
 </template>
@@ -19,7 +28,56 @@ export default {
 
   data() {
     return {
-      feriados: []
+      feriados: [],
+      days: [
+        {text: '1', id: 1}, 
+        {text: '2', id: 2}, 
+        {text: '3', id: 3}, 
+        {text: '4', id: 4}, 
+        {text: '5', id: 5},
+        {text: '6', id: 6},
+        {text: '7', id: 7},
+        {text: '8', id: 8},
+        {text: '9', id: 9},
+        {text: '10', id: 10},
+        {text: '11', id: 11},
+        {text: '12', id: 12},
+        {text: '13', id: 13},
+        {text: '14', id: 14},
+        {text: '15', id: 15},
+        {text: '16', id: 16},
+        {text: '17', id: 17},
+        {text: '18', id: 18},
+        {text: '19', id: 19},
+        {text: '20', id: 20},
+        {text: '21', id: 21},
+        {text: '22', id: 22},
+        {text: '23', id: 23},
+        {text: '24', id: 24},
+        {text: '25', id: 25},
+        {text: '26', id: 26},
+        {text: '27', id: 27},
+        {text: '28', id: 28},
+        {text: '29', id: 29},
+        {text: '30', id: 30},
+        {text: '31', id: 31}
+      ],
+      months: [
+        {text: 'Janeiro', id: 1}, 
+        {text: 'Fevereiro', id: 2}, 
+        {text: 'Março', id: 3}, 
+        {text: 'Abril', id: 4}, 
+        {text: 'Maio', id: 5},
+        {text: 'Junho', id: 6},
+        {text: 'Julho', id: 7},
+        {text: 'Agosto', id: 8},
+        {text: 'Setembro', id: 9},
+        {text: 'Outubro', id: 10},
+        {text: 'Novembro', id: 11},
+        {text: 'Dezembro', id: 12}
+      ],
+      selectedMonth: '',
+      selectedDay: '',
     }
   },
 
@@ -39,13 +97,30 @@ export default {
       return format(new Date(`${year}/${month}/${day}`), 'dd/MM/yyyy')
     },
 
+    filtrarDia(day) {
+      feriados.listar().then(resposta => {
+        const dia = resposta.data.response.holidays
+
+        this.feriados = dia.filter(dia => dia.date.datetime.day == day)
+      });
+    },
+
     filtrarMes(month) {
+      console.log(month)
       feriados.listar().then(resposta => {
         const mes = resposta.data.response.holidays
 
         this.feriados = mes.filter(mes => mes.date.datetime.month == month)
       });
-    }
+    },
+    
+    // filtrarTipo(type) {
+    //   feriados.listar().then(resposta => {
+    //     const tipo = resposta.type
+
+    //     this.feriados = tipo.filter(tipo => tipo.type == type)
+    //   });
+    // }
   }
 }
 </script>
