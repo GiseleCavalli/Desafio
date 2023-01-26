b65da2d0b174e4d2687bc34e85467921c8219a63
GiseleCavalli <giselecavalli28@hotmail.com>
2023-01-25 21:42:31 -0300

    Finalizando calendario

---
+++ service/variables.js
@@ -0,0 +1,51 @@
+
+const days = [
+    {text: '1', id: 1}, 
+    {text: '2', id: 2}, 
+    {text: '3', id: 3}, 
+    {text: '4', id: 4}, 
+    {text: '5', id: 5},
+    {text: '6', id: 6},
+    {text: '7', id: 7},
+    {text: '8', id: 8},
+    {text: '9', id: 9},
+    {text: '10', id: 10},
+    {text: '11', id: 11},
+    {text: '12', id: 12},
+    {text: '13', id: 13},
+    {text: '14', id: 14},
+    {text: '15', id: 15},
+    {text: '16', id: 16},
+    {text: '17', id: 17},
+    {text: '18', id: 18},
+    {text: '19', id: 19},
+    {text: '20', id: 20},
+    {text: '21', id: 21},
+    {text: '22', id: 22},
+    {text: '23', id: 23},
+    {text: '24', id: 24},
+    {text: '25', id: 25},
+    {text: '26', id: 26},
+    {text: '27', id: 27},
+    {text: '28', id: 28},
+    {text: '29', id: 29},
+    {text: '30', id: 30},
+    {text: '31', id: 31}
+]
+
+const months = [
+    {text: 'Janeiro', id: 1}, 
+    {text: 'Fevereiro', id: 2}, 
+    {text: 'Março', id: 3}, 
+    {text: 'Abril', id: 4}, 
+    {text: 'Maio', id: 5},
+    {text: 'Junho', id: 6},
+    {text: 'Julho', id: 7},
+    {text: 'Agosto', id: 8},
+    {text: 'Setembro', id: 9},
+    {text: 'Outubro', id: 10},
+    {text: 'Novembro', id: 11},
+    {text: 'Dezembro', id: 12}
+]
+
+export { days, months };
--- src/App.vue
+++ src/App.vue
@@ -35,59 +35,16 @@
 <script>
 import feriados from '../service/feriado';
 import { format } from 'date-fns';
+import { days, months } from '../service/variables';
+import './assets/app.css';
 
 export default {
 
   data() {
     return {
       feriados: [],
-      days: [
-        {text: '1', id: 1}, 
-        {text: '2', id: 2}, 
-        {text: '3', id: 3}, 
-        {text: '4', id: 4}, 
-        {text: '5', id: 5},
-        {text: '6', id: 6},
-        {text: '7', id: 7},
-        {text: '8', id: 8},
-        {text: '9', id: 9},
-        {text: '10', id: 10},
-        {text: '11', id: 11},
-        {text: '12', id: 12},
-        {text: '13', id: 13},
-        {text: '14', id: 14},
-        {text: '15', id: 15},
-        {text: '16', id: 16},
-        {text: '17', id: 17},
-        {text: '18', id: 18},
-        {text: '19', id: 19},
-        {text: '20', id: 20},
-        {text: '21', id: 21},
-        {text: '22', id: 22},
-        {text: '23', id: 23},
-        {text: '24', id: 24},
-        {text: '25', id: 25},
-        {text: '26', id: 26},
-        {text: '27', id: 27},
-        {text: '28', id: 28},
-        {text: '29', id: 29},
-        {text: '30', id: 30},
-        {text: '31', id: 31}
-      ],
-      months: [
-        {text: 'Janeiro', id: 1}, 
-        {text: 'Fevereiro', id: 2}, 
-        {text: 'Março', id: 3}, 
-        {text: 'Abril', id: 4}, 
-        {text: 'Maio', id: 5},
-        {text: 'Junho', id: 6},
-        {text: 'Julho', id: 7},
-        {text: 'Agosto', id: 8},
-        {text: 'Setembro', id: 9},
-        {text: 'Outubro', id: 10},
-        {text: 'Novembro', id: 11},
-        {text: 'Dezembro', id: 12}
-      ],
+      days: days,
+      months: months,
       selectedMonth: '',
       selectedDay: '',
     }
@@ -125,55 +82,10 @@ export default {
     limparFiltro(){
       this.selectedDay = '';
       this.selectedMonth = '';
+      feriados.listar().then(resposta => {
+        this.feriados = resposta.data.response.holidays
+      });
     }
   }
 }
 </script>
-
-<style>
-body{
-  background-color: #f5ebe5;
-  color: #625240;
-}
-
-.p{
-  font-size: 17.5px;
-}
-
-.div{
-  display: flex;
-  justify-content: space-evenly;
-  margin-bottom: 5%;
-}
-
-.button{
-  height: 38px;
-  margin-top: 65%;
-  padding: 5px 15px 5px 15px;
-  font-size: 18px;
-  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-  border-radius: 6px;
-  border: 1px solid #63513c;
-  background-color: #9fccc3;
-  color: #63513c;
-  cursor: pointer;
-}
-
-.select{
-  padding: 5px 15px 5px 15px;
-  margin-right: 5%;
-  font-size: 16px;
-  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-  border-radius: 6px;
-  border: 1px solid #63513c;
-  background-color: #ebddd5;
-  color: #63513c;
-  cursor: pointer;
-}
-
-.holiday{
-  font-size: 17px;
-  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-  text-align: left;
-}
-</style>
